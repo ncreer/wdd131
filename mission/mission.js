f66e7a794ed0deb6aa09aa28aa3e8c95ae54ab2b
ncreer <natekc@byui.edu>
2026-05-02 16:53:56 -0600

    assignment

---
+++ mission/mission.js
@@ -0,0 +1,19 @@
+
+let selectElem = document.querySelector('select');
+let logo = document.querySelector('img');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    if (current == 'dark') {
+        document.body.classList.add('dark');
+//        document.document-container.classList.add('dark');
+        logo.setAttribute('src', 'https://wddbyui.github.io/wdd131/images/byui-logo-white.png');
+    } else {
+        document.body.classList.remove('dark');
+//        document.document-container.classList.remove('dark');
+        logo.setAttribute('src', 'https://wddbyui.github.io/wdd131/images/byui-logo-blue.webp');
+    }
+}           
+                    
